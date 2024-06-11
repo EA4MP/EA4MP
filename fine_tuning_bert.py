163b1e7271e289b44eaef181103ccdcd06a49fd7
EA4MP <mx120230566@stu.yzu.edu.cn>
2024-06-11 19:21:47 +0800

    Update fine_tuning_bert.py

--- fine_tuning_bert.py
+++ fine_tuning_bert.py
@@ -1,3 +1,7 @@
+# you can run this script is you dont have a GPU ,but it may very very very slow
+# so we recommend using another script : fine_tune_with_GPU.py
+
+
 from transformers import BertForSequenceClassification, Trainer, TrainingArguments
 from torch.utils.data import TensorDataset, DataLoader
 import torch
@@ -7,31 +11,28 @@ from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained(r"F:\TrainClassfier\bert-base-cased")
 model = BertForSequenceClassification.from_pretrained(r"F:\TrainClassfier\bert-base-cased", num_labels=2)
 
-# 设置微调参数
+
 # training_args = TrainingArguments(
-#     output_dir="./results",   # 指定微调结果输出路径
-#     num_train_epochs=3,       # 设置微调的轮数
-#     per_device_train_batch_size=8,  # 指定训练时每个GPU/CPU设备的批大小
-#     logging_dir="./logs",     # 指定训练日志输出路径
+#     output_dir="./results",   
+#     num_train_epochs=3,       
+#     per_device_train_batch_size=8,  
+#     logging_dir="./logs",     
 # )
 
-# 数据文件路径
-data_path = r'F:\TrainClassfier\datasets\train.txt'
+
+data_path = r'...\train.txt'
 
 
-# 预处理数据
 def preprocess_data(data_path):
     inputs = []
     labels = []
 
-    # 读取数据文件
     with open(data_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
         for line in lines:
-            # 分割文本和标签
             parts = line.strip().split(',label:')
-            text = parts[0].strip()  # 获取文本部分
-            label = int(parts[1].strip())  # 获取标签部分
+            text = parts[0].strip()  
+            label = int(parts[1].strip()) 
             inputs.append(text)
             labels.append(label)
 
@@ -40,7 +41,6 @@ def preprocess_data(data_path):
 
 inputs, labels = preprocess_data(data_path)
 
-# 将输入数据和标签转换为Tensor对象
 inputs = tokenizer(inputs, padding=True, truncation=True, max_length=512, return_tensors='pt')
 labels = torch.tensor(labels)
 
@@ -59,4 +59,4 @@ for epoch in range(8):
         optimizer.step()
         optimizer.zero_grad()
 
-model.save_pretrained(r"./my_classifier_model")
+model.save_pretrained(r".../my_classifier_model")
