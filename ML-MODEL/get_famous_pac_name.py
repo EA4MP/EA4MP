3eb429c5b9948025dd7a0cea386374bfeef13d62
EA4MP <mx120230566@stu.yzu.edu.cn>
2024-06-12 20:45:37 +0800

    Update get_famous_pac_name.py

--- ML-MODEL/get_famous_pac_name.py
+++ ML-MODEL/get_famous_pac_name.py
@@ -4,8 +4,7 @@ import json
 import pypistats
 from pprint import pprint
 
-# 获取每个pypi包的下载次数，并将下载次数超过100000次的软件包作为流行包，作为后续验证是否存在typosquatting的基准
-csv_file_name = r"F:\TrainClassfier\src\pac_all_pypi.csv"
+csv_file_name = r"pac_all_pypi.csv"
 with open(csv_file_name, "r") as file:
     reader = csv.reader(file)
     for row in reader:
@@ -14,9 +13,8 @@ with open(csv_file_name, "r") as file:
             data = pypistats.overall(pac_name, mirrors=False, format="json")
             data = json.loads(data)
             downloads = data["data"][0]["downloads"]
-            if downloads > 100000:
+            if downloads > 1000:
                 print(pac_name)
-                # 将流行包的包名存储到famous.csv文件中去
                 famous_csv_file_name = r"F:\TrainClassfier\src\NBProcess\famous.csv"
                 with open(famous_csv_file_name, "a") as famous_file:
                     famous_file.write(pac_name + "\n")
