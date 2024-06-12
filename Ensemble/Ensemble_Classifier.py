f00b1f06678dafb421ec4c1b6d4ce58b56b6b6e2
EA4MP <mx120230566@stu.yzu.edu.cn>
2024-06-12 20:50:40 +0800

    Update Ensemble_Classifier.py

--- Ensemble/Ensemble_Classifier.py
+++ Ensemble/Ensemble_Classifier.py
@@ -7,20 +7,19 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 
-# 加载Naive Bayes模型
-with open(r'F:\TrainClassfier\src\NBProcess\naive_bayes_model.pkl', 'rb') as f:
+with open('naive_bayes_model.pkl', 'rb') as f:
     nb_model = pickle.load(f)
 
-# 加载微调的BERT模型
-tokenizer = BertTokenizer.from_pretrained(r'F:\TrainClassfier\bert-base-cased')
-bert_model = BertForSequenceClassification.from_pretrained(r'F:\TrainClassfier\my_classifier11_model')
+
+tokenizer = BertTokenizer.from_pretrained(r'bert-base-cased')
+bert_model = BertForSequenceClassification.from_pretrained(r'')
 bert_model.eval()
 
-# 加载数据
-with open(r'F:\TrainClassfier\src\NBProcess\all_info_data_set.txt', 'r') as f:
+
+with open(r'', 'r') as f:
     lines = f.readlines()
 
-# 读取和处理数据
+
 X_combined = []
 y = []
 
@@ -35,32 +34,29 @@ for line in lines:
     input_ids = tokenizer(text, return_tensors="pt", padding=True, truncation=True)['input_ids']
     bert_outputs = bert_model(input_ids)['logits'].detach().numpy()[0]
 
-    nb_pred = nb_model.predict_proba([nb_features[:-1]])[0][1]  # 取正类的概率
+    nb_pred = nb_model.predict_proba([nb_features[:-1]])[0][1] 
 
-    combined_features = [bert_outputs[1], nb_pred]  # 取正类的概率或决策函数值
+    combined_features = [bert_outputs[1], nb_pred] 
     X_combined.append(combined_features)
     y.append(label)
 
 X_combined = np.array(X_combined)
 y = np.array(y)
 
-# 划分训练集和测试集
+
 X_train, X_test, y_train, y_test = train_test_split(X_combined, y, test_size=0.2, random_state=42)
 
-# 训练Adaboost
 adaboost = AdaBoostClassifier(n_estimators=50, algorithm='SAMME')
 adaboost.fit(X_train, y_train)
 
-# 评估Adaboost集成模型性能
 predictions = adaboost.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 report = classification_report(y_test, predictions)
 
-print("测试集准确率:", accuracy)
-print("测试集分类报告：\n", report)
+print("accuary:", accuracy)
+print("report：\n", report)
 
-# 保存Adaboost集成模型
 with open('adaboost_ensemble_model.pkl', 'wb') as f:
     pickle.dump(adaboost, f)
 
-print("模型已保存。")
+print("save finish")
