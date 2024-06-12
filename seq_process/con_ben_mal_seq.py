f77105f69d85e4fc3d499502955c00a75fafcd9a
EA4MP <mx120230566@stu.yzu.edu.cn>
2024-06-12 20:20:14 +0800

    Update con_ben_mal_seq.py

--- seq_process/con_ben_mal_seq.py
+++ seq_process/con_ben_mal_seq.py
@@ -1,26 +1,24 @@
+
+# You can mix the extracted malicious code behavior sequences with the benign code behavior sequences after tagging to facilitate the fine-tuning of the BERT model
+
 def merge_text_files(file1_path, file2_path, output_path):
-    # 打开第一个文件并读取内容
     with open(file1_path, 'r', encoding='utf-8') as file1:
         file1_content = file1.read()
 
-    # 打开第二个文件并读取内容
     with open(file2_path, 'r', encoding='utf-8') as file2:
         file2_content = file2.read()
 
-    # 将两个文件的内容合并
     merged_content = file1_content + '\n' + file2_content
 
-    # 将合并后的内容写入新文件
+
     with open(output_path, 'w', encoding='utf-8') as output_file:
         output_file.write(merged_content)
 
 
-# 指定两个输入文件和输出文件的路径
-file1_path = r'F:\TrainClassfier\datasets\ben\ben_bfs.txt'
-file2_path = r'F:\TrainClassfier\datasets\mal\mal_bfs.txt'
-output_path = r'F:\TrainClassfier\datasets\train.txt'
+file1_path = r''
+file2_path = r''
+output_path = r''
 
-# 合并两个文件
 merge_text_files(file1_path, file2_path, output_path)
 
-print("文件合并完成。")
+print("file incorporation finish!")
