dc2a101c13d5b4eecc8b9a2e9fe52cff3d4aa69f
EA4MP <mx120230566@stu.yzu.edu.cn>
2024-06-12 20:42:22 +0800

    Update delete_break_file.py

--- ML-MODEL/delete_break_file.py
+++ ML-MODEL/delete_break_file.py
@@ -5,7 +5,7 @@ import shutil
 def is_file_corrupted(file_path):
     try:
         with open(file_path, 'rb') as f:
-            f.read()  # 尝试读取文件内容，如果文件损坏可能会抛出异常
+            f.read() 
     except Exception as e:
         print(f"File {file_path} is corrupted: {e}")
         return True
@@ -20,11 +20,11 @@ def delete_corrupted_files(directory):
             if is_file_corrupted(file_path):
                 print(f"Deleting corrupted file: {file_path}")
                 try:
-                    shutil.rmtree(file_path)  # 使用shutil.rmtree删除文件或目录
+                    shutil.rmtree(file_path)  
                 except Exception as e:
                     print(f"Error deleting file {file_path}: {e}")
 
 
-# 使用示例
-delete_corrupted_files(r'/home/banxiangao/Desktop/DataSet/dcp_malSample')
+
+delete_corrupted_files(r'')
 
