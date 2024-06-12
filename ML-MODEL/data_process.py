4b3052a35ae40e8e528422008db40f484c6e60c4
EA4MP <mx120230566@stu.yzu.edu.cn>
2024-06-12 20:41:58 +0800

    Update data_process.py

--- ML-MODEL/data_process.py
+++ ML-MODEL/data_process.py
@@ -2,19 +2,16 @@ import ast
 
 
 def parse_line(line):
-    # 根据逗号和空格分割数据
+
     parts = line.split(' ,')
-    # 第一部分是代码序列，包含CLS和SEP
+
     code_sequence = parts[0]
-    # 第二部分是特征向量
+
     feature_vector_str = ast.literal_eval(parts[1])
     feature_vector = feature_vector_str[:-1]
-    # 第三部分提取最后一个数字作为标签
+
     label = feature_vector_str[-1]
-    # print(feature_vector)
-    # print(label)
-    # 将特征向量转换为整数列表
-    # feature_vector = list(map(int, feature_vector_str.split()[:-1]))
+
     return code_sequence, feature_vector, label
 
 
